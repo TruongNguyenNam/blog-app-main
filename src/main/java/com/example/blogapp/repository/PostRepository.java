1a707853804edb9e5ae512b191ee06d0f62137c3
TruongNguyenNam <truongnnph49746@gmail.com>
2024-11-11 14:37:03 +0700

    update repository

---
+++ src/main/java/com/example/blogapp/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.example.blogapp.repository;
+
+
+import com.example.blogapp.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findByCategoryId(Long categoryId);
+}
